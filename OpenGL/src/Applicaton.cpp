8c18885ed46afe7cc3082b23f7225c9046c88886
Roadinforest <whuforest@outlook.com>
2025-07-05 14:48:36 +0800

    Finished S08 , created a vertex shader and a fragment shader in a raw way.

--- OpenGL/src/Applicaton.cpp
+++ OpenGL/src/Applicaton.cpp
@@ -3,6 +3,54 @@
 #include "GLFW/glfw3.h"
 #include "examples.h"
 
+/* The kernel combination function */
+static unsigned int CompileShader(unsigned int type ,const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str(); // This will be danger if we the source string is invalid or free later
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cerr << " Failed to compile " <<
+			(type==GL_VERTEX_SHADER ? "vertex" : "fragment") 
+			<< " shader! " << std::endl;
+		std::cout << message << std::endl;
+
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+	 
+}
+
+/* It is basically create a vertex shader and fragment shader using the source text */
+static unsigned int CreateShaderProgram(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER,vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER,fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 /* Just set the vertex and draw it without using buffer*/
 void drawSimpleTriangle()
 {
@@ -40,6 +88,7 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 
+	/* ---------- Set the OpenGL buffer ---------- */
 	float pos[6] = {
 		-0.5f, +0.0f,
 		+0.5f, -0.5f,
@@ -55,9 +104,38 @@ int main(void)
 
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), pos, GL_STATIC_DRAW);
 
+	/* -------------------------------------------- */
+
 	/* Set the vertex attribute */
-	glEnableVertexAttribArray(0); /* You can enable it at any line, not necessary to be front of glVertexAttribPointer */
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0);
+	unsigned int AttribIndex = 0;
+	glEnableVertexAttribArray(AttribIndex); /* You can enable it at any line, not necessary to be front of glVertexAttribPointer */
+	glVertexAttribPointer(AttribIndex, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0);
+
+	/* ---------- Create a program with two shaders ---------- */
+	std::string vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location =0) out vec4 color;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"}\n";
+
+	unsigned int program = CreateShaderProgram(vertexShader, fragmentShader);
+	glUseProgram(program);
+
+	/* --------------------------------------------------------- */
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -72,6 +150,8 @@ int main(void)
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	glDeleteProgram(program);
 
 	glfwTerminate();
 	return 0;
