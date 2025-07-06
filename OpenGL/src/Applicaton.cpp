5a739f6e40e730266a0d3c2a2d38b683e2037cf7
Roadinforest <whuforest@outlook.com>
2025-07-06 14:46:54 +0800

    Added and used vertex array object .

--- OpenGL/src/Applicaton.cpp
+++ OpenGL/src/Applicaton.cpp
@@ -136,6 +136,11 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
+	/* Spefify the OpenGL version */
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -157,7 +162,7 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 
-	/* ---------- Set the OpenGL buffer ---------- */
+	/* ---------- Set the OpenGL buffer : vertex buffer and indice buffer ---------- */
 	float pos[] = {
 		-0.5f, -0.5f,
 		+0.5f, -0.5f,
@@ -170,6 +175,11 @@ int main(void)
 		2, 3, 0
 	};
 
+	/* Use the vertex array object */
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
+
 	/* Create vertex buffer */
 	unsigned int vertex_buffer;
 	GLCall(glGenBuffers(1, &vertex_buffer));
@@ -178,19 +188,20 @@ int main(void)
 	/* Create a OpenGL Buffer object */
 	GLCall(glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), pos, GL_STATIC_DRAW));
 
+	/* ---------- Set the vertex attribute--------- */
+	unsigned int AttribIndex = 0;
+	GLCall(glEnableVertexAttribArray(AttribIndex)); /* You can enable it at any line, not necessary to be front of glVertexAttribPointer */
+	GLCall(glVertexAttribPointer(AttribIndex, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0));
+	/* -------------------------------------------- */
+
 	/* Create indice buffer */
 	unsigned int indice_buffer;
 	GLCall(glGenBuffers(1, &indice_buffer));
 	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indice_buffer);) /* Vertex array indices */
 	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
-	/* -------------------------------------------- */
+	/* ------------------------------------------------------------------------------- */
 
-	/* ---------- Set the vertex attribute--------- */
-	unsigned int AttribIndex = 0;
-	GLCall(glEnableVertexAttribArray(AttribIndex)); /* You can enable it at any line, not necessary to be front of glVertexAttribPointer */
-	GLCall(glVertexAttribPointer(AttribIndex, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0));
-	/* -------------------------------------------- */
 
 	/* ---------- Create a program with two shaders ---------- */
 
@@ -207,7 +218,7 @@ int main(void)
 	/* --------------------------------------------------------- */
 
 	float r = 0.0f;
-	float increment = 0.0001f;
+	float increment = 0.01f;
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -222,6 +233,9 @@ int main(void)
 
 		r += increment;
 
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+		//GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);) /* Vertex array indices */
+		GLCall(glBindVertexArray(vao));
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
